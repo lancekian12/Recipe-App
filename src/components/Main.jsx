3a5a476ebdf5b733ba2f395579293ea11fa1a03c
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-05-11 21:38:50 +0800

    handling submit in a old way using event listener for testing purposes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,32 @@
 import React from 'react'
 
 const Main = () => {
+    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        ingredients.push(newIngredient)
+        console.log(ingredients)
+    }
+
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form className="add-ingredient-form" onSubmit={handleSubmit}>
                 <input
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
                 />
                 <button>Add ingredient</button>
+                <ul>
+                    {ingredientsListItems}
+                </ul>
             </form>
         </main>
     )
