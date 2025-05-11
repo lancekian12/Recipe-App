0c40209cc4b6f8bd5eb44199e81a6a61038a0ad6
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-05-11 21:01:15 +0800

    added the header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import chefClaudeLogo from "../assets/Chef Claude Icon.png"
+
+
+
+const Header = () => {
+    return (
+        <header>
+            <img src={chefClaudeLogo} />
+            <h1>Chef Claude</h1>
+        </header>)
+}
+
+export default Header
