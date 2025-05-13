7cf32874304dbd8035c2121e9892773b6b2bf90a
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-05-13 14:57:29 +0800

    learned how to use state in object

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/Header'
 import Main from './components/Main'
 // import State from './components/State'
+import StateObject from './components/StateObject'
 function App() {
 
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Main />
       {/* <State /> */}
+      <StateObject />
     </>
   )
 }
