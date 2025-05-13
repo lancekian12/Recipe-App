872e102bb19135e616ed3f1d7b2ec4ca92a84a48
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-05-13 15:41:21 +0800

    relearn form submission

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 
 const Form = () => {
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formEl = event.currentTarget
+        const formData = new FormData(formEl)
+        const email = formData.get("email")
+        console.log(email)
+        // Gather the info from the form
+        // Submit it to a backend
+        formEl.reset()
+    }
     return (
+
         <section>
             <h1>Signup form</h1>
-            <form>
+            <form onSubmit={handleSubmit} method="post">
                 <label htmlFor="email">Email:</label>
                 <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
                 <br />
@@ -13,8 +24,7 @@ const Form = () => {
                 <input id="password" type="password" name="password" />
                 <br />
 
-                <input type="submit" />
-
+                <button>Submit</button>
 
             </form>
         </section>
