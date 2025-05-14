255cd690ea851b53f9167b220693fadc0f8625a1
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-05-14 19:43:18 +0800

    using radio in updated form react 19

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,8 +4,10 @@ const Form = () => {
     function signUp(formData) {
         const email = formData.get("email")
         const password = formData.get("password")
+        const employmentStatus = formData.get("employmentStatus")
         console.log(email)
         console.log(password)
+        console.log(employmentStatus)
     }
     return (
 
@@ -21,6 +23,22 @@ const Form = () => {
 
                 <label htmlFor="description">Description:</label>
                 <textarea id="description" name="description"></textarea>
+                <fieldset>
+                    <legend>Employment Status:</legend>
+                    <label>
+                        <input type="radio" name="employmentStatus" value="unemployed" />
+                        Unemployed
+                    </label>
+                    <label>
+                        <input type="radio" name="employmentStatus" value="part-time" />
+                        Part-time
+                    </label>
+                    <label>
+                        <input type="radio" name="employmentStatus" defaultChecked="true" value="full-time" />
+                        Full-time
+                    </label>
+                </fieldset>
+
 
                 <button>Submit</button>
 
