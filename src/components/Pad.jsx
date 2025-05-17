f80837e812f64c0f013e330f43ba04fdeefad2ce
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-05-18 00:25:39 +0800

    conditonal rendering in className

--- src/components/Pad.jsx
+++ src/components/Pad.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 const Pad = (props) => {
+    console.log(props.data.on)
     const styles = {
         backgroundColor: props.data.color
     }
     return (
-        <button style={styles} key={props.data.id}></button>
+        <button className={props.data.on ? "on" : "button-custom"}
+            style={styles} key={props.data.id}></button>
     )
 }
 
